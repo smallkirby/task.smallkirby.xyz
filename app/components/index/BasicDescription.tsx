617899d2fa9cefed268eb55ecd06979c2a4bfadd
smallkirby <ssmallkirby@gmail.com>
2022-07-26 20:40:47 +0900

    💄 minor index fix

--- app/components/index/BasicDescription.tsx
+++ app/components/index/BasicDescription.tsx
@@ -1,11 +1,11 @@
 export default function BasicDescription() {
   return (
-    <div className="flex flex-col md:flex-row md:pt-4">
-      <div className="md:w-1/2 flex flex-col px-10">
+    <div className="flex flex-col-reverse md:flex-row md:pt-4">
+      <div className="w-full md:w-1/2 flex flex-col px-5 md:px-10">
         <div>
           <div className="flex mx-auto mb-5">
             <img src='/img/sample/task_list.png' alt='task list' className="rounded-2xl mr-5 w-1/2" />
-            <img src='/img/sample/task_progress.png' alt='task progress' className="rounded-2xl w-1/2" />
+            <img src='/img/sample/task_progress.png' alt='task progress' className="rounded-2xl mr-5 w-1/2" />
           </div>
         </div>
         <div className="mx-auto">
@@ -13,7 +13,7 @@ export default function BasicDescription() {
         </div>
 
       </div>
-      <div className="md:w-1/2 px-10 my-auto py-auto">
+      <div className="w-full md:w-1/2 px-4 md:px-10 mb-8 md:my-auto md:py-auto">
         <div className="text-4xl md:text-6xl font-oswald font-bold leading-relaxed">
           <h2>
             <span className="text-skblue">List</span>
